1fe436de3a6a11feeddfc051aa5ef629f0f36103
joesephorekhov <josephorekhov@gmail.com>
2021-08-13 19:10:32 -0600

    commit tree 54358467ed10c49709cc6b337d0131766764b046

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+// function superbowlWin(x){
+//     const winningObj = x.find(obj => (obj.result === 'W'))
+//       if(winningObj === undefined){
+//           return undefined;
+//       }else(winningObj.result === 'W')
+//         return winningObj.year
+// }
+  
+function superbowlWin(objArr){
+    const winningObj = objArr.find(obj => (obj.result === 'W'))
+         if(winningObj.result === 'W'){
+           return winningObj.year
+         }
+   }
