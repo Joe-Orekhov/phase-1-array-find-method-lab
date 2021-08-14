3c57543a96d7349cfab8d4992645cbdc60dea660
joesephorekhov <josephorekhov@gmail.com>
2021-08-13 19:12:07 -0600

    Compleated Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(x){
+    const winningObj = x.find(obj => (obj.result === 'W'))
+      if(winningObj === undefined){
+          return undefined;
+      }else(winningObj.result === 'W')
+        return winningObj.year
+}
